68015c979fd68dc8212c0ba3ab4b7543d3042ea7
derrick.jiang <derrick.jiang@71360.com>
2021-05-08 15:18:31 +0800

    feat: 配置husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,14 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
-  extends: ['plugin:vue/essential', 'airbnb-base'],
+  extends: ['plugin:vue/essential', 'airbnb-base', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 12,
     parser: '@typescript-eslint/parser',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['vue', '@typescript-eslint'],
-  rules: {},
-};
+  rules: {}
+}
