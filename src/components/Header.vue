d9e0d20318a2551eda1ab4180741c16712256f2d
Yixiu1996 <jczcycy@163.com>
2021-05-24 23:16:27 +0800

    feat: 添加头部组件展示时间功能

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-05-20 22:12:59
- * @LastEditTime: 2021-05-20 22:39:59
+ * @LastEditTime: 2021-05-24 23:16:01
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue3-element-admin\src\components\Header.vue
@@ -11,13 +11,20 @@
     <div class="left">
       <span style="font-size: 20px">{{ name }}</span>
     </div>
-    <div class="right">右</div>
+    <div class="right">
+      <div class="right-icon">
+        <i :class="iconfont"></i>
+      </div>
+      <div class="right-time">{{ timeValue }}</div>
+    </div>
   </div>
 </template>
 
 <script>
-import { reactive, toRefs } from 'vue'
+import { reactive, toRefs, watch} from 'vue'
 import { useRouter } from 'vue-router'
+import dayjs from 'dayjs'
+
 export default {
   name: 'Header',
   setup() {
@@ -27,12 +34,33 @@ export default {
       add: '添加商品'
     }
     const state = reactive({
-      name: '首页'
+      name: '首页',
+      timeValue:'',
+      timer: null,
+      iconfont: '&#xe690;'
     })
     router.afterEach((to) => {
-      console.log('to', to)
-      const { id } = to.query
+      // const { id } = to.query
       state.name = pathMap[to.name]
+    })
+    if(state.timer) clearInterval()
+    state.timer = setInterval(()=>{
+      state.timeValue = dayjs().format('YYYY/DD/MM HH:MM:ss')
+      // console.log(dayjs().format('HH'))
+    }, 1000)
+    watch(state, () => {
+      if(dayjs().format('HH') >= 0 && dayjs().format('HH') < 6){
+        state.iconfont = 'iconfont icon-lingchen'
+      } else if(dayjs().format('HH') >= 6 && dayjs().format('HH') < 12){
+        state.iconfont = 'iconfont icon-shangwu'
+      } else if(dayjs().format('HH') >= 12 && dayjs().format('HH') < 18){
+        state.iconfont = 'iconfont icon-xiawu'
+      } else {
+        state.iconfont = 'iconfont icon-wanshang'
+      }
+    }, {
+      // 页面加载会先执行一次
+      immediate: true
     })
     return {
       ...toRefs(state)
@@ -41,7 +69,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .header {
   height: 50px;
   border-bottom: 1px solid #e9e9e9;
@@ -49,5 +77,14 @@ export default {
   justify-content: space-between;
   align-items: center;
   padding: 0 20px;
+  .right {
+    display: flex;
+  }
+  .iconfont{
+    color: #FF8000;
+  }
+  .right-time{
+    margin-left:15px;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,20 @@
 /*
  * @Author: your name
  * @Date: 2021-05-20 21:06:48
- * @LastEditTime: 2021-05-20 21:53:11
+ * @LastEditTime: 2021-05-24 22:52:43
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue3-element-admin\src\main.js
  */
-import { createApp } from 'vue'
+import {
+  createApp
+} from 'vue'
 import App from './App.vue'
 import router from '@/router'
 import ElementPlus from 'element-plus'
 // import 'element-plus/lib/theme-chalk/index.css';
 import '~/theme/index.css'
+import '@/assets/iconfont/iconfont.css'
 
 const app = createApp(App)
 
